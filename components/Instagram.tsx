e3d78907a6852d5c400996aa9663990435c02ee0
jsonwamuyu <jysonmuchiri@gmail.com>
2023-02-23 22:14:19 -0800

    Done with Instagram section

--- components/Instagram.tsx
+++ components/Instagram.tsx
@@ -3,15 +3,16 @@ import InstagramImg from './InstagramImg'
 
 const Instagram = () => {
   return (
-    <section className='flex p-4 max-w-[1240px] m-auto flex-col items-center justify-center'>
-        <p className="text-xl text-center font-semibold">Follow Me on Instagram</p>
-        <p className='text-md text-gray-500'>@Capture</p>
-        <div className='mt-6 flex flex-col sm:flex-row flex-wrap space-x-4'>
+    <section className='flex p-4 py-24 text-center max-w-[1240px] m-auto flex-col items-center justify-center'>
+        <p className="text-2xl font-bold">Follow Me on Instagram</p>
+        <p className='text-md text-gray-500 pb-4'>@Capture</p>
+        <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4'>
+            <InstagramImg socialImg='/images/shoes.png'/>
+            <InstagramImg socialImg='/images/greens.jpg'/>
+            <InstagramImg socialImg='/images/plate.jpg'/>
             <InstagramImg socialImg='/images/shoes.png'/>
             <InstagramImg socialImg='/images/shoes.png'/>
-            <InstagramImg socialImg='/images/shoes.png'/>
-            <InstagramImg socialImg='/images/shoes.png'/>
-            {/* <InstagramImg socialImg='/images/shoes.png'/> */}
+            <InstagramImg socialImg='/images/food.jpg'/>
         </div>
     </section>
   )
--- components/InstagramImg.tsx
+++ components/InstagramImg.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import Image from 'next/image';
-import { FaInstagram } from 'react-icons/fa';
+// import React from 'react'
+// import Image from 'next/image';
+// import { FaInstagram } from 'react-icons/fa';
  
-const InstagramImg = ({socialImg}) => {
-  return (
-    <div className='relative'>
-      <Image src={socialImg} alt="Instangram" height={200} width={300} className="rounded-lg w-full h-full"/>
-      {/* Overlay div */}
-      <div className='flex items-center justify-center absolute top-0 left-0 bottom-0 right-0 hover:bg-black/50 cursor-pointer group'/>
-      <p className='text-blue-500 hidden group-hover:block'><FaInstagram size={20}/></p>
+// const InstagramImg = ({socialImg}:{socialImg:string}) => {
+//   return (
+//     <div className='relative'>
+//       <Image src={socialImg} alt="Instangram" height={200} width={250} className="rounded-lg w-full h-full hover:rounded-lg"/>
+//       {/* Overlay div */}
+//       <div className='flex items-center justify-center absolute top-0 left-0 bottom-0 right-0 hover:bg-black/50 cursor-pointer group'>
+//       <p className='text-gray-300 hidden group-hover:block'><FaInstagram size={30}/></p>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default InstagramImg
+
+import Image from "next/image";
+
+
+const InstagramImg = ({socialImg} : {socialImg:string}) =>{
+  return(
+    <div className="relative">
+      <Image src={socialImg} alt="instagram" height={200} width={250} className="w-full h-full rounded-lg"/>
+      <div className="rounded-lg top-0 bottom-0 left-0 right-0 hover:bg-black/50 group">
+        <p className="text-gray-300 hidden group-hover:block"><FaInstagram size={30} className="z-10"/></p>
+      </div>
     </div>
   )
 }
 
-export default InstagramImg
+export default InstagramImg;
